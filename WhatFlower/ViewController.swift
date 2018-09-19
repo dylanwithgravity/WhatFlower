1f3058e8156469fa14874aaede78ef6a0d801b68
Dylan Williamson <dylanwithgravity@gmail.com>
2018-09-19 18:45:52 -0400

    Add files via upload

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+      
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+        
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {
+                
+                fatalError("Cannot convert to CIImage.")
+            }
+            
+            detect(image: convertedCIImage)
+        
+        imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            
+            fatalError("Cannot import Model")
+            
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            
+            
+            self.navigationItem.title = classification?.identifier.capitalized
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+        try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+        
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
     }
-
-
+    
 }
 
